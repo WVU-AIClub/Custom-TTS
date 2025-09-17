9c629f099745e71be9a5cc2aa1ef9042ba039c47
SeanCarp <swcarpenter04@gmail.com>
2025-09-17 13:03:03 -0400

    Inference Init

---
+++ inference.py
@@ -0,0 +1,16 @@
+# pip install piper-tts
+import wave
+from piper import PiperVoice, SynthesisConfig
+
+syn_config = SynthesisConfig(
+    volume=0.5,  # half as loud
+    length_scale=2.0,  # twice as slow
+    noise_scale=1.0,  # more audio variation
+    noise_w_scale=1.0,  # more speaking variation
+    normalize_audio=False, # use raw audio from voice
+)
+
+voice = PiperVoice.load("/path/to/en_US-lessac-medium.onnx")
+with wave.open("test.wav", "wb") as wav_file:
+    voice.synthesize_wav("Welcome to the world of speech synthesis!", \
+                         wav_file, syn_config=syn_config)
