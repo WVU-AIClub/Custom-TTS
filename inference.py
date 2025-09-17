e68073b85787daea455261ec8453284cc04f6e77
SeanCarp <swcarpenter04@gmail.com>
2025-09-17 16:39:19 -0400

    Nate Voice

--- inference.py
+++ inference.py
@@ -3,14 +3,14 @@ import wave
 from piper import PiperVoice, SynthesisConfig
 
 syn_config = SynthesisConfig(
-    volume=0.5,  # half as loud
-    length_scale=2.0,  # twice as slow
-    noise_scale=1.0,  # more audio variation
-    noise_w_scale=1.0,  # more speaking variation
+    volume=7,  # half as loud
+    length_scale=0.75,  # twice as slow
+    noise_scale=0.5,  # more audio variation
+    noise_w_scale=1,  # more speaking variation
     normalize_audio=False, # use raw audio from voice
 )
 
-voice = PiperVoice.load("voices/peter_griffin1.onnx")
-with wave.open("test.wav", "wb") as wav_file:
-    voice.synthesize_wav("Welcome to the world of speech synthesis!", \
+voice = PiperVoice.load("voices/nate_model.onnx")
+with wave.open("Nate_Tongue.wav", "wb") as wav_file:
+    voice.synthesize_wav("UNIQUE NEW YORK UNEED NEW YORK YOU KNOW UNEED UNIQUE NEW YORK", \
                          wav_file, syn_config=syn_config)
