193a563cad069c32be8cbbbebe1fde4abdef20de
SeanCarp <swcarpenter04@gmail.com>
2025-11-23 19:55:49 -0500

    DONE. Please Upload

--- inference.py
+++ inference.py
@@ -1,16 +1,15 @@
-# pip install piper-tts
 import wave
 from piper import PiperVoice, SynthesisConfig
 
 syn_config = SynthesisConfig(
-    volume=7,  # half as loud
-    length_scale=0.75,  # twice as slow
-    noise_scale=0.5,  # more audio variation
-    noise_w_scale=1,  # more speaking variation
+    volume=0.5,  # half as loud
+    length_scale=2.0,  # twice as slow
+    noise_scale=1.0,  # more audio variation
+    noise_w_scale=1.0,  # more speaking variation
     normalize_audio=False, # use raw audio from voice
 )
-
-voice = PiperVoice.load("voices/nate_model.onnx")
-with wave.open("Nate_Tongue.wav", "wb") as wav_file:
-    voice.synthesize_wav("UNIQUE NEW YORK UNEED NEW YORK YOU KNOW UNEED UNIQUE NEW YORK", \
+# TODO: Bug fix with loading json. Must be  .onnx.json
+voice = PiperVoice.load("Peter/PETAH")
+with wave.open("Thanksgiving.wav", "wb") as wav_file:
+    voice.synthesize_wav("Happy Thanks,giving from A.I.W.V.U!", \
                          wav_file, syn_config=syn_config)
