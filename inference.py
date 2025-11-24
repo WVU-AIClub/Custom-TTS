971e19ae832ddb2b6851a825fe6ae067712b7271
aiwvu <wvuaic@gmail.com>
2025-11-24 10:43:15 -0500

    Nate Trained

--- inference.py
+++ inference.py
@@ -3,13 +3,13 @@ from piper import PiperVoice, SynthesisConfig
 
 syn_config = SynthesisConfig(
     volume=0.5,  # half as loud
-    length_scale=2.0,  # twice as slow
+    length_scale=1.4,  # twice as slow
     noise_scale=1.0,  # more audio variation
-    noise_w_scale=1.0,  # more speaking variation
+    noise_w_scale=1.5,  # more speaking variation
     normalize_audio=False, # use raw audio from voice
 )
 # TODO: Bug fix with loading json. Must be  .onnx.json
-voice = PiperVoice.load("Peter/PETAH")
-with wave.open("Thanksgiving.wav", "wb") as wav_file:
-    voice.synthesize_wav("Happy Thanks,giving from A.I.W.V.U!", \
+voice = PiperVoice.load("Nate/Nate")
+with wave.open("Nate_6650.wav", "wb") as wav_file:
+    voice.synthesize_wav("Hello, I am Nate. Please give me more money.", \
                          wav_file, syn_config=syn_config)
